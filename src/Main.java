9dfb990e8d64d3e29eae40bda64acdb47b6a606b
Alex00000 <angara00000@yandex.ru>
2023-02-19 18:52:48 +0300

    Homework2.1.2

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 74; // вес в килограммах
+        double height = 1.75; // рост в метрах
+        double bmi = service.calculate(weight, height); // считаем ИМТ
+        String bmiResult = String.format("%.2f",bmi); // округляем до 2-х знаков
+        System.out.println("Ваш вес: " + weight + " кг.");
+        System.out.println("Ваш рост: " + height*100 + " см.");
+        System.out.println("Ваш индекс массы тела: " + bmiResult);
+    }
+}
